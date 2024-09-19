cbe87a3579f1bb0a83214ad83bd660637f1c238c
Jiri Sveceny <jiri.sveceny@icloud.com>
2024-09-19 10:39:59 +0200

    chore: removed css, we dont need that
    Signed-off-by: Jiri Sveceny <jiri.sveceny@icloud.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './globals.css'
-
 const LandingPage = () => (
-    <div className={'flex flex-col h-screen justify-center items-center'}>
-        <header className='pt-10 pb-5'>
-            <div className='container mx-auto text-center'>
-                <h1 className='text-6xl font-bold'>Jiří Svěcený</h1>
-                <p className='text-3xl mt-4'>
-                    Helping startups sail safely through the sea
-                </p>
+    <header>
+        <center>
+            <h1>Jiří Svěcený</h1>
+            <h2>Helping startups sail safely through the sea</h2>
+            <h3>
+                <a href='mailto:jiri.sveceny@icloud.com'>
+                    jiri.sveceny@icloud.com
+                </a>
+            </h3>
+        </center>
 
-                <div className={'mt-4 grid gap-2 grid-cols-1'}>
-                    <a href='mailto:jiri.sveceny@icloud.com' className='text-2xl text-gray-700 mt-6 block hover:underline border border-gray-300 px-4 py-2 inline-block rounded'>
-                        jiri.sveceny@icloud.com
-                    </a>
-                    <a href='https://github.com/sitole' className='text-2xl text-gray-700 mt-6 block hover:underline border border-gray-300 px-4 py-2 inline-block rounded'>
-                        github.com/sitole
-                    </a>
-                    <a href='https://x.com/jirisveceny' className='text-2xl text-gray-700 mt-6 block hover:underline border border-gray-300 px-4 py-2 inline-block rounded'>
-                        twitter.com/jirisveceny
-                    </a>
-                </div>
-            </div>
-        </header>
-    </div>
+        <hr/>
+
+        <center>
+            <p>
+                <a href='https://github.com/sitole'>https://github.com/sitole</a>
+            </p>
+            <p>
+                <a href='https://x.com/jirisveceny'>https://twitter.com/jirisveceny</a>
+            </p>
+        </center>
+    </header>
 )
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
